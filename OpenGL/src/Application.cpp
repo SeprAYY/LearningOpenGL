1207406c9982b75de74247aa3d3970846d47d54d
Julian Łabanowski <julian.labanowski@wp.pl>
2023-07-03 10:38:54 +0200

    index buffers

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -111,16 +111,27 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+    float positions[] = {
+        -0.5f, -0.5f, //0
+         0.5f, -0.5f, //1
+         0.5f,  0.5f, //2
+        -0.5f,  0.5f  //3
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int buffer;//buffer id
     glGenBuffers(1, &buffer); //buffer creation
     glBindBuffer(GL_ARRAY_BUFFER, buffer); //buffer selection
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2  * sizeof(float), positions, GL_STATIC_DRAW);
+
+    unsigned int ibo;//index buffer object
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
@@ -137,7 +148,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //index buffer is binded so we can nullptr
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); 
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
