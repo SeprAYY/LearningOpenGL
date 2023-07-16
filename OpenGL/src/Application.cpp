d9e02c119149bc66a29c0b9883abd5124de27663
Julian Łabanowski <julian.labanowski@wp.pl>
2023-07-16 21:09:19 +0200

    need to fix errors

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,38 +1,15 @@
-#include<GL/glew.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 
-#define DEBUG = 1
-#define ASSERT(x) if(!(x)) __debugbreak();
+#include "Renderer.h"
 
-#ifdef  DEBUG
-	#define GLCall(x) GLClearError();\
-	x;\
-	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-#else
-	#define GLCall(x) x
-#endif
-
-
-static void GLClearError()
-{
-	while (glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-	while (GLenum error = glGetError())
-	{
-		std::cout << "[OpenGL Error] (" << error << ")" << function <<
-			" " << file << ":" << line << std::endl;
-		return false;
-	}
-
-	return true;
-}
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 struct ShaderProgramSource
 {
@@ -162,10 +139,7 @@ int main(void)
 	GLCall(glGenVertexArrays(1, &vao)); //generate 1 vertex array object
 	GLCall(glBindVertexArray(vao));
 
-	unsigned int buffer;//buffer id
-	GLCall(glGenBuffers(1, &buffer)); //buffer creation
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer)); //buffer selection
-	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
+	VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
 	GLCall(glEnableVertexAttribArray(0));
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
@@ -173,10 +147,7 @@ int main(void)
 	//before last param-> amount we need to go forward to get to second vertex
 	//last param -> amount we need to go forward to get to second attribute
 
-	unsigned int ibo;//index buffer object
-	GLCall(glGenBuffers(1, &ibo));
-	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+	IndexBuffer ib(indices, 6);
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -204,14 +175,14 @@ int main(void)
 		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
 		GLCall(glBindVertexArray(vao));
-		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+		ib.Bind();
 
 		//index buffer is binded so we can nullptr
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		if (r > 1.0f)
 			increment = -0.05f;
-		else if(r < 0.0f)
+		else if (r < 0.0f)
 			increment = 0.05f;
 
 		r += increment;
