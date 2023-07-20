196a8a65c50f56c0b0c532892784371ed479f6d2
Julian Łabanowski <julian.labanowski@wp.pl>
2023-07-20 19:00:49 +0200

    Basic Renderer

--- OpenGL/src/Renderer.cpp
+++ OpenGL/src/Renderer.cpp
@@ -17,3 +17,18 @@ bool GLLogCall(const char* function, const char* file, int line)
 
 	return true;
 }
+
+void Renderer::Clear() const
+{
+	GLCall(glClear(GL_COLOR_BUFFER_BIT));
+}
+
+void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const
+{
+	shader.Bind();
+	va.Bind();
+	ib.Bind();
+
+	//index buffer is binded so we can nullptr
+	GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
+}
